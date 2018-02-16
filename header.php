d0b1d597016e75b413fbc8adfe2d9bc9e3c47299
slicepotato <m.ianjohnson@gmail.com>
2018-02-16 00:41:46 -0500

    nightly build revision

--- header.php
+++ header.php
@@ -13,10 +13,10 @@
   <meta property="og:image" content="http://www.derbycitydesignlab.com" />
 
 	<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
-  <link href="<?php echo get_bloginfo('template_directory'); ?>/css/style.min.css" rel="stylesheet">
 	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
 
 	<?php wp_head();?>
+	<link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
 </head>
 
 <body <?php if(!is_page_template( 'splash.php')) { body_class('page'); } else { body_class(); } ?>>
